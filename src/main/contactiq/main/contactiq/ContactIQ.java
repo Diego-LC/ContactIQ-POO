f44d4926f852990f9e4d8ea0c436cc40bd7a96dd
DIΣGӨ L <di3golabrin@gmail.com>
2023-09-11 20:59:38 -0300

    menú plantilla de ejemplo inicial

--- src/main/contactiq/main/contactiq/ContactIQ.java
+++ src/main/contactiq/main/contactiq/ContactIQ.java
@@ -1,9 +1,9 @@
 package main.contactiq;
-import java.io.Scanner;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ContactIQ {
-    public static void main(String[] args) {
+    public static <Contacto> void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Contacto> contactos = new ArrayList<>();
         int opcion = 0;
@@ -23,32 +23,17 @@ public class ContactIQ {
                     String correo = sc.next();
                     System.out.println("Ingrese el teléfono: ");
                     String telefono = sc.next();
-                    Contacto contacto = new Contacto(nombre, correo, telefono);
-                    contactos.add(contacto);
+                    System.out.println(telefono);
                     break;
                 case 2:
-                    for (Contacto c : contactos) {
-                        System.out.println(c);
-                    }
                     break;
                 case 3:
                     System.out.println("Ingrese el nombre a buscar: ");
                     String nombreBuscado = sc.next();
-                    for (Contacto c : contactos) {
-                        if (c.getNombre().equals(nombreBuscado)) {
-                            System.out.println(c);
-                        }
-                    }
                     break;
                 case 4:
                     System.out.println("Ingrese el nombre a eliminar: ");
                     String nombreEliminar = sc.next();
-                    for (Contacto c : contactos) {
-                        if (c.getNombre().equals(nombreEliminar)) {
-                            contactos.remove(c);
-                            break;
-                        }
-                    }
                     break;
                 case 5:
                     System.out.println("Gracias por usar ContactIQ");
