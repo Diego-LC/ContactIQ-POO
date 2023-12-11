4624e0d0ce1be3104d3075ec0c31d7f2eb508fad
DIΣGӨ L <di3golabrin@gmail.com>
2023-12-10 22:16:34 -0300

    Añadido Javadoc

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controlador"},{"l":"datos"},{"l":"main"},{"l":"model"},{"l":"principal"},{"l":"ventanas"}];updateSearchResults();
